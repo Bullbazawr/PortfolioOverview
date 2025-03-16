1e0f8c9ec5d1dd7bfdeabd8b0148951d4a264a29
Jack <Ev.ydu@yandex.ru>
2025-03-16 05:28:25 +0100

    implemented state preservation in localstorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cryptoReducer from './cryptoSlice'
 
+const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem('cryptoPortfolio')
+        if (serializedState === null) {
+            return undefined
+        }
+        return JSON.parse(serializedState)
+    } catch (err) {
+        return undefined
+    }
+}
+
+const saveState = (state: RootState) => {
+    try {
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('cryptoPortfolio', serializedState)
+    }catch {
+        console.error('Error saving state to localStorage')
+    }
+}
+
 const store = configureStore({
     reducer: cryptoReducer,
+    preloadedState: loadState(),
+})
+
+store.subscribe(() => {
+    saveState(store.getState())
 })
 
 export type RootState = ReturnType<typeof store.getState>
